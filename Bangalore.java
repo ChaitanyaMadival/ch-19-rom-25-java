84fa85f2ee43903154251afa222822627ed40de7
ChaitanyaMadival <chaitanyamadival277@gmail.com>
2025-07-02 16:20:43 +0530

    committing Externally initialized array files

---
+++ Bangalore.java
@@ -0,0 +1,46 @@
+class Bangalore {
+	/*
+	static String jaynagar = "Jayanagar";
+	static String indiranagar = "Indiranagar";
+	static String whitefield = "Whitefield";
+	static String kormangala = "Koramangala";
+	static String hrsLayout = "HSR Layout";
+	static String malleshwaram = "Malleshwaram";
+	static String rajajinagar = "Rajajinagar";
+	static String banashankari = "Banashankari";
+	static String electronicCity = "Electronic City";
+	static String bannergatta = "Bannerghatta Road";
+	static String basavanagudi = "Basavanagudi";
+	static String sadashivaNagara = "Sadashivanagar";
+	
+	static String areaNames[] = {jaynagar, indiranagar, whitefield, kormangala, hrsLayout, malleshwaram, rajajinagar, 
+		banashankari, electronicCity, bannergatta, basavanagudi, sadashivaNagara};
+	*/
+	public static void main(String areas[]) {
+	
+	System.out.println("Main Started");
+	//	System.out.println(areaNames[0]+ " "+areaNames[1]+ " "+areaNames[2]+ " "+areaNames[3]+ " "+areaNames[4]+ " "+areaNames[5]+ " "+
+	//		areaNames[6]+ " "+areaNames[7]+ " "+areaNames[8]+ " "+areaNames[9]+ " "+areaNames[10]+ " "+areaNames[11]);
+		 String jaynagar = "Jayanagar";
+		 String indiranagar = "Indiranagar";
+		 String whitefield = "Whitefield";
+		 String kormangala = "Koramangala";
+		 String hrsLayout = "HSR Layout";
+		 String malleshwaram = "Malleshwaram";
+		 String rajajinagar = "Rajajinagar";
+		 String banashankari = "Banashankari";
+		 String electronicCity = "Electronic City";
+		 String bannergatta = "Bannerghatta Road";
+		 String basavanagudi = "Basavanagudi";
+		 String sadashivaNagara = "Sadashivanagar";
+		
+		 String areaNames[] = {jaynagar, indiranagar, whitefield, kormangala, hrsLayout, malleshwaram, rajajinagar, 
+			banashankari, electronicCity, bannergatta, basavanagudi, sadashivaNagara};
+			
+		for(String areaName:areaNames) {
+			System.out.println(areaName);
+		}
+		System.out.println("Main Ended");
+	}
+
+}
